c9dee80e76ae5e6ddec3c89a8f8d4df8e3298d03
Victor <VictorCarmona9035@hotmail.com>
2023-06-04 10:17:15 +0200

    234-Re-establecer y establecer valor al formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,29 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+//Creamos una constante que seran los valores que tendra nuestro formulario
+const propiedadesInicialesFormulario ={
+  name: 'Victor',
+  price: 0,
+  insStorage: 0
+}
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
   styleUrls: ['./basic-page.component.css']
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
   constructor(private fb: FormBuilder) {
 
   }
+  ngOnInit(): void {
+    this.myForm.reset(propiedadesInicialesFormulario);
+  }
 
   public myForm: FormGroup = this.fb.group({
-    name: ['',[Validators.required, Validators.minLength(3)]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
     insStorage: [0, [Validators.required, Validators.min(0)]]
   })
 
   onSave(): void {
 
-    if(this.myForm.invalid){
+    if (this.myForm.invalid) {
       return;
     }
     console.log(this.myForm.value);
+    //Restablecer el formulario parte 1
+    //this.myForm.reset();
+
+    //Restablecer formulario parte 2
+    //this.myForm.reset({ price: 10, insStorage: 0 });
   }
 
 }
